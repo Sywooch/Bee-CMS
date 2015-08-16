a2d3cdab768f3d0e9a6b0042da9f8904b5326cd5
OGreSiv <intfom@gmail.com>
2015-08-16 12:19:23 +0300

    Update _db.php

--- common/config/_db.php
+++ common/config/_db.php
@@ -4,9 +4,9 @@
  */
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=promobv.mysql.ukraine.com.ua;dbname=promobv_bee',
-    'username' => 'promobv_bee',
-    'password' => '32jcmcf2',
+    'dsn' => 'mysql:host=localhost',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
     'tablePrefix' => 'feq2d_'
 ];
